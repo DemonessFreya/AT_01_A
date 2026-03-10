8e95ee40e7c0df2191e4244425005a22be73db4a
Freyja <freyamarika@gmail.com>
2026-03-11 03:43:38 +1100

    Add test array and check for minimum number.
    Test array replaces old array; assertion added to test if function finds 2 as the minimum number in the array.

--- AT_01_A/minimum_element.cpp
+++ AT_01_A/minimum_element.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <cassert>
 
 int minElement();
 
 int main() {
+	assert(minElement() == 2, "Minimum element is 2");
+
 	std::cout << minElement() << std::endl;
 
 	return 0;
 }
 
 int minElement() {
-	int arr[] = { 1, 3, 2, 5 };
+	int arr[] = { 67,13,3,89,43,2,19,71,5,61,97,7,37,31,17,11,83,53,23,29 };
 
 	int min = arr[0];
 
